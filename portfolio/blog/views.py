c099441d61ff2678bc8f1664ffd8eac40d6a843a
jayashri <jaysharipatil01@gmail.com>
2025-07-16 12:23:06 +0530

    blog app create

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Article
+
+class BlogListView(ListView):
+    model = Article
+    template_name = "blog/blog_list.html"
+    def get_queryset(self):
+        return Article.objects.all().order_by('-created_at')
+
+
+class BlogDetailView(DetailView):
+    model = Article
+    template_name = "blog/blog_detail.html"
--- portfolio/website/views.py
+++ portfolio/website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def Index(request):
+    return render(request, template_name = "website/index.html")
+
+
